61cdc8b6992ecf2e270356f3b9b0c618286373bb
armandhe <armandhenewpy@163.com>
2021-08-07 22:26:44 +0800

    bcbvc

---
+++ match_domain.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+# @Author   : ArmandHe
+# @Time     : 2021/8/4 11:21
+# @Project  : match_domain
+# @File     : match_domain.py
+# @SoftWare : PyCharm
+
+from os import system
+from os import listdir
+
+for filename in listdir("../chengdu"):
+    system(f"python3 subdomain_find.py < ../chengdu/{filename} > ../chengdure/{filename}.txt")
+
